eaa7128150bcfdc833a227cc44bbf9dffcb1108c
itZhy <zhy1987819@163.com>
2013-10-15 23:51:11 +0800

    add file md5 calculation example for csharp.

---
+++ C#/MD5Calculation/FileMD5.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.IO;
+
+namespace MD5Calculation
+{
+    class FileMD5
+    {
+        public string Calculate(string filePath)
+        {
+            using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] md5data = md5.ComputeHash(stream);
+                return System.BitConverter.ToString(md5data).Replace("-", "");
+            }
+        }
+    }
+}
---
+++ C#/MD5Calculation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MD5Calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String filePath = getFilePath();
+            Console.WriteLine("该文件的MD5值为：");
+            try
+            {
+                Console.WriteLine(new FileMD5().Calculate(filePath));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("读取文件：[" + filePath + "]失败，原因：" + ex.Message);
+            }
+        }
+
+        private static String getFilePath()
+        {
+            Console.WriteLine("请输入需要计算MD5值的文件路径。");
+            return Console.ReadLine();
+        }
+    }
+}
